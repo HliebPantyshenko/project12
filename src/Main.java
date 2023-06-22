2031a5f18b4979f88718a731ad92ba1f36d81c77
Hlieb Pantyushenko <pantushenko11@gmail.com>
2023-06-22 13:26:01 +0300

    menu item

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final int EXIT_NUMBER = 0;
     public static void main(String[] args) {
-        System.out.println("Hello worl");
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+        while (isRunning){
+            int menuItem = scanner.nextInt();
+            if (menuItem == EXIT_NUMBER){
+                isRunning = false;
+            }
+        }
+
     }
 }
