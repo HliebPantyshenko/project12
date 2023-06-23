94dcf0350836e2f6014beb0da68f2c859ce182cf
Hlieb Pantyushenko <pantushenko11@gmail.com>
2023-06-23 15:31:17 +0300

    Created decode method in CeasedCypher class

--- src/CeasedCypher.java
+++ src/CeasedCypher.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class CeasedCypher {
@@ -11,11 +12,23 @@ public class CeasedCypher {
     private static final String NAME_FILE = "Укажіть ім'я нового файлу: ";
     private static final String FILE_CREATED = "Файл створено!";
     private static final String FILE_WROTE_TO_FILE = "Наше таємне повідомлення записано в файл";
-    private static final String STILL_WOORKING = """
+    private static final String STILL_WORKING =
+            """
             Бажаєте продовжити?
             Введіть "так" або "ні"
             """;
-
+    private static final String IS_KEY =
+            """
+            Чи маєте ви ключ?
+            Введіть "так", якщо маєте.
+            Інші варіанти розглядаються як те, що ви не маєте ключа.
+            """;
+    private static final String SELECT_KEY = "Введіть ключ: ";
+    private static final String IS_READABLE =
+            """
+            Чи можете ви прочитати текст?
+            Введіть "так", якщо можете.
+            """;
     public static void encode(String fileName){
         System.out.println(REAL_MESSAGE);
         readMessage(fileName);
@@ -34,15 +47,20 @@ public class CeasedCypher {
         System.out.println(FAKE_MESSAGE);
         readMessage(fileName);
 
-        System.out.println(REAL_MESSAGE);
-        //String encodeMessage = encodeProcess();
+        int key = isKey();
 
+        System.out.println(REAL_MESSAGE);
+        String decodeMessage = decodeProcess(key);
+
+        creatingNewFile(decodeMessage);
+
+        message = "";
         isStillRunning();
     }
 
     private static boolean existSymbol(int number){
-        for (int i = 0; i < MUTABLE_CHARACTERS.length; i++) {
-            if(MUTABLE_CHARACTERS[i] == number){
+        for (int mutableCharacter : MUTABLE_CHARACTERS) {
+            if (mutableCharacter == number) {
                 return true;
             }
         }
@@ -62,21 +80,61 @@ public class CeasedCypher {
                 counter +=1;
                 message = message + line;
             }
-            System.out.println(message);
+            System.out.println(message = message.toLowerCase());
             bufferedReader.close();
         }catch (IOException e) {
                 e.printStackTrace();
         }
     }
+    private static String decodeProcess(int key){
+        if (key == -1){
+            return noKeyDecodeProcess();
+        }
+        return decodeProcessAlgorithm(key);
+    }
 
+    private static String noKeyDecodeProcess(){
+        boolean isRunning = true;
+        int key = 0;
+        while (isRunning){
+            System.out.println(IS_READABLE);
+            String decodedText = decodeProcessAlgorithm(key);
+            System.out.println(decodedText);
+            Scanner scanner = new Scanner(System.in);
+            String answer = scanner.nextLine();
+            if(answer.equals("так")) {
+                isRunning = false;
+                return decodedText;
+            }
+            key+=1;
+        }
+        return null;
+    }
+
+    private static String decodeProcessAlgorithm(int key){
+        String new_str = "";
+        for (int i = 0; i < message.length(); i++) {
+            int characterIndex = message.charAt(i);
+            if (characterIndex - key >= 1072 && characterIndex - key <= 1103) {
+                int changedSymbol = (message.charAt(i) - key);
+                int normalValue = (changedSymbol - 1072) % 32 + 1072;
+                new_str += (char) normalValue;
+            } else if (existSymbol(characterIndex - key)) {
+                new_str += (char) (characterIndex - key);
+            } else {
+                new_str += (char) (characterIndex);
+            }
+        }
+        return new_str;
+    }
     private static String encodeProcess(){
         int key = 3;
         String new_str = "";
         for (int i = 0; i < message.length(); i++) {
             int characterIndex = message.charAt(i);
-            if(characterIndex >= 1040 && characterIndex <= 1071){
+            if(characterIndex >= 1072 && characterIndex <= 1103){
                 int changedSymbol = (message.charAt(i) + key);
-                int normalValue = (changedSymbol - 1040) % 32 + 1040;
+                int normalValue = (changedSymbol - 1072) % 32 + 1072;
                 new_str += (char) normalValue;
             } else if(existSymbol(characterIndex)){
                 new_str += (char) (characterIndex + key);
@@ -110,10 +168,20 @@ public class CeasedCypher {
             e.printStackTrace();
         }
     }
-
+    private static int isKey(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(IS_KEY);
+        String answer = scanner.nextLine();
+        if(answer.equals("так")){
+            System.out.println(SELECT_KEY);
+            int number = scanner.nextInt();
+            return number;
+        }
+        return -1;
+    }
     private static void isStillRunning(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println(STILL_WOORKING);
+        System.out.println(STILL_WORKING);
         String answer = scanner.nextLine();
         switch (answer){
             case "так" -> new Menu().run();
